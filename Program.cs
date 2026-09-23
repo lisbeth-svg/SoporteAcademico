30a8e70b1de017ac674ed96f329b5930f8c27929
Lisbeth <lisswtx@gmail.com>
2026-09-22 20:56:57 -0500

    Req. 1: registra datos basicos de solicitud

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string codigo;
+        string nombre;
+        string tipo;
+        string descripcion;
+
+        Console.WriteLine("SISTEMA DE SOPORTE ACADÉMICO");
+        Console.WriteLine("-----------------------------");
+
+        Console.Write("Ingrese código de estudiante: ");
+        codigo = Console.ReadLine();
+
+        Console.Write("Ingrese nombre: ");
+        nombre = Console.ReadLine();
+
+        Console.Write("Ingrese tipo de consulta: ");
+        tipo = Console.ReadLine();
+
+        Console.Write("Ingrese descripción: ");
+        descripcion = Console.ReadLine();
+    }
+}
